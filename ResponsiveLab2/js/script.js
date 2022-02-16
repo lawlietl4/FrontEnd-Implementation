3cfbdf716cc347fa080725a37fe87a11a8a94199
Nicholas Roberson <nroberson7712@gmail.com>
2022-02-16 11:22:02 -0700

    more work being done

---
+++ ResponsiveLab2/js/script.js
@@ -0,0 +1,24 @@
+let url = `json/config.json`;
+
+const main = data => {
+    console.log(data);
+    let navbar = document.getElementsByClassName('navbar');
+    let menu_contents = '';
+    data.menu_items.forEach(item => {
+        console.log(item);
+        menu_contents += `
+        <div>
+            <a href='${item.path}'>
+                ${item.name}
+            </a>
+        </div
+        `;
+    });
+    navbar[0].innerHTML = menu_contents;
+};
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        main(data);
+    });
