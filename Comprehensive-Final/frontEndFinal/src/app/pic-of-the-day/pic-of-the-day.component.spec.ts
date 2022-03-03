8866ee920390e1eee09785f6c1fe2038ded79851
Nicholas Roberson <nroberson7712@gmail.com>
2022-03-02 19:00:11 -0700

    starting final

---
+++ Comprehensive-Final/frontEndFinal/src/app/pic-of-the-day/pic-of-the-day.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PicOfTheDayComponent } from './pic-of-the-day.component';
+
+describe('PicOfTheDayComponent', () => {
+  let component: PicOfTheDayComponent;
+  let fixture: ComponentFixture<PicOfTheDayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PicOfTheDayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PicOfTheDayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
